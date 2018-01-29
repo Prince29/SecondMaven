7d3080284f6e144425024d2a916f58b2bb5b33a0
suxiang <xsu@leqee.com>
2018-01-29 15:08:54 +0800

    多moudle项目

--- web-common/src/main/java/com/xsu/web/common/client/WebServerCilent.java
+++ web-common/src/main/java/com/xsu/web/common/client/WebServerCilent.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -20,17 +22,20 @@ public class WebServerCilent {
 
     public <T extends BaseResponse> T execute (BaseRequest<T> request,int timeout) throws IOException{
         if(timeout<0)timeout=0;
+        T response=newResponseInstance(request);
+
         URLConnection conn=null;
         try {
             URL url=new URL(urlStr);
             conn=url.openConnection();
             conn.setConnectTimeout(connectTimeout);
             conn.setReadTimeout(timeout);
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        conn.setRequestProperty("Content-Type", "application/octet-stream");
+
 
         conn.connect();
 
@@ -50,4 +55,28 @@ public class WebServerCilent {
         //reurn resultBuffer.toString();
         return null;
     }
+    private <T extends BaseResponse> T newResponseInstance(BaseRequest request)throws IOException{
+        Class<T> clazz=getResponseType(request);
+        try{
+            return clazz.newInstance();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private <T extends BaseResponse> Class<T> getResponseType(BaseRequest request)throws IOException{
+        Class<?> requestClass=request.getClass();
+        Type superType=requestClass.getGenericSuperclass();
+        if(!(superType instanceof ParameterizedType)){
+            throw new IOException("getResponseType error 1");
+        }
+        ParameterizedType type=(ParameterizedType)superType;
+        Type[] responseTypes=type.getActualTypeArguments();
+        if(responseTypes.length!=1){
+            throw  new IOException("getResposeType error 2");
+        }
+        Class<T> clazz=(Class<T>)responseTypes[0];
+        return clazz;
+    }
 }
+
